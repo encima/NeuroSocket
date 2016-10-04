3895fffb4a7d6cf65948de81afed785d7863a2d4
encima <encima@gmail.com>
2016-10-04 14:46:57 +0100

    applescript issues, foreground app now retrieved using pyobjc

--- pysocket.py
+++ pysocket.py
@@ -11,15 +11,13 @@ from multiprocessing import Process
 import re
 import config
 import argparse
-
-#from Foundation import *
-#from ScriptingBridge import *
+from AppKit import NSWorkspace
+import osx.OSXActiveApp as osaa
 
 pp = pprint.PrettyPrinter(indent=4)
-
 parser = argparse.ArgumentParser(description='Log all the productivity')
 parser.add_argument('-o','--output',help='Output file name', required=False)
-parser.add_argument('-d','--dbname',help='DB name', required=False, default=config.DB_NAME)
+parser.add_argument('-d','--dbname',help='DB name', required=False, default=None)
 parser.add_argument('-i','--interval',help='Interval for readings', required=False, default=30, type=int)
 parser.add_argument("-m", "--mindwave", help="Connect to mindwave", action="store_true")
 args = parser.parse_args()
@@ -27,17 +25,19 @@ server = couchdb.Server()
 server.resource.credentials = (config.DB_USERNAME, config.DB_PWD)
 db = None
 
-try:
-    db = server[args.dbname]
-except:
-    db = server.create(args.dbname)
-print("DB Connected")
+if args.dbname is not None:
+    try:
+        db = server[args.dbname]
+    except:
+        db = server.create(args.dbname)
+    print("DB Connected")
 LOG_NAME = "output/log_{0}.json"
 HOST_INFO = platform.uname()
 print("Running on {}".format(HOST_INFO.system))
 
 def save_reading(reading):
-    db.save(reading)
+    if db is not None:
+        db.save(reading)
 
 def get_app(host):
     foreground_app = None
@@ -51,16 +51,18 @@ def get_app(host):
         foreground_app = re.split("= |\n", foreground_app)
         foreground_app = {'program': foreground_app[3], 'title': foreground_app[1]}
     elif host == 'Darwin':
-        foreground_app = json.loads(foreground_app)
+        foreground_app = osaa.OSXActiveApp.getActive()
+        foreground_app = foreground_app
     elif host == 'Windows':
         foreground_app = json.loads(foreground_app)
-    return foreground_app 
+    return foreground_app
 
 def enrich_reading(d_json):
     d_json['time'] = str(datetime.now())
     d_json['host'] = HOST_INFO.node
     d_json['app'] = get_app(HOST_INFO.system)
     d_json['platform'] = HOST_INFO.system
+    print(d_json)
 
 sock = None
 if args.mindwave:
@@ -70,7 +72,7 @@ if args.mindwave:
     print('connecting to %s port %s' % server_address)
     sock.connect(server_address)
 try:
-    # Send config message to mindwave 
+    # Send config message to mindwave
     if sock:
         message = "{\"enableRawOutput\": false, \"format\": \"Json\"}\n";
     # print('sending "%s"' % message)
@@ -84,11 +86,11 @@ try:
             print(dataform)
             try:
                 struct = json.loads(dataform)
-            
+
                 if struct:
                     if ('status' in struct and struct['status'] != "scanning") or 'status' not in struct:
                         d_json = struct
-                        #add time and foreground app to json 
+                        #add time and foreground app to json
                         enrich_reading(d_json)
 #                pp.pprint(d_json)
                         if config.BUFFER:
@@ -100,7 +102,7 @@ try:
                                     p.join()
                                 readings = []
                         else:
-                            save_reading(d_json) 
+                            save_reading(d_json)
             except Exception as e:
                 print("------")
                 print(str(e))
