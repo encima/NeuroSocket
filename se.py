aa7e3eda128a7f57eceea1dee64458c06b58d753
gwilliams_c <christopher.gwilliams@psi.ch>
2017-11-18 15:22:31 +0100

    we have readings! (albeit with hard coded key checks)

--- se.py
+++ se.py
@@ -23,7 +23,6 @@ CODES = {
     'ASIC_EEG_POWER': {'code': b'\x83', 'length': 24}
 }
 
-
 port = "/dev/rfcomm0"
 ser = serial.Serial(port, 38400)
 s = ser.read(100)
@@ -41,6 +40,7 @@ def parse_payload(payload):
     """
     print(payload)
     code = ord(payload[0])
+    print(code)
     if code in CODES:
         print(code)
         length = CODES[code]['length']
@@ -52,7 +52,120 @@ def parse_payload(payload):
             print(decode(payload[1:1+length]))
         parse_payload(payload[1 + length:])
     else:
-        Print("Unknown code")
+        print("Unknown code")
+
+def handle_load(payload):
+    i = 0
+    while i < len(payload):
+        code = payload[i]
+        # parse_payload(payload)
+        if (code == 'd0'):
+            print("Headset connected!")
+        elif (code == 'd1'):
+            print("Headset not found, reconnecting")
+        elif(code == 'd2'):
+            print("Disconnected!")
+        elif(code == 'd3'):
+            print("Headset denied operation!")
+        elif(code == 'd4'):
+            if payload[2] == 0:
+                print("Idle, trying to reconnect")
+        elif(code == CODES['POOR_SIGNAL']['code']):  # poorSignal
+            i = i + 1
+            poor_signal = int(payload[i].hex(), 16)
+            print('POOR SIGNAL', poor_signal)
+        elif(code == CODES['ATTENTION']['code']):  # attention
+            i = i + 1
+            attention = int(payload[i].hex(), 16)
+            print('ATTENTION', attention)
+        elif(code == CODES['MEDITATION']['code']):  # meditation
+            i = i + 1
+            meditation = int(payload[i].hex(), 16)
+            print('MEDITATION', meditation)
+        elif(code == CODES['BLINK']['code']):  # blink strength
+            i = i + 1
+            blink_strength = int(payload[i].hex(), 16)
+            print('BLINK', blink_strength)
+        elif(code == CODES['RAW_VALUE']['code']):  # raw value
+            i = i + 1  # for length/it is not used since length =1 byte long and always=2
+            i = i + 1
+            val0 = int(payload[i].hex(), 16)
+            i = i + 1
+            raw_value = val0 * 256 + int(payload[i].hex(), 16)
+            if raw_value > 32768:
+                raw_value = raw_value - 65536
+            # print('RAW', raw_value)
+        elif(code == CODES['ASIC_EEG_POWER']['code']):  # ASIC_EEG_POWER
+            w = {}
+            i = i + 1  # for length/it is not used since length =1 byte long and always=2
+            # delta:
+            i = i + 1
+            val0 = int(payload[i].hex(), 16)
+            i = i + 1
+            val1 = int(payload[i].hex(), 16)
+            i = i + 1
+            w['delta'] = val0 * 65536 + \
+                val1 * 256 + int(payload[i].hex(), 16)
+            # theta:
+            i = i + 1
+            val0 = int(payload[i].hex(), 16)
+            i = i + 1
+            val1 = int(payload[i].hex(), 16)
+            i = i + 1
+            w['theta'] = val0 * 65536 + \
+                val1 * 256 + int(payload[i].hex(), 16)
+            # lowAlpha:
+            i = i + 1
+            val0 = int(payload[i].hex(), 16)
+            i = i + 1
+            val1 = int(payload[i].hex(), 16)
+            i = i + 1
+            w['lowAlpha'] = val0 * 65536 + \
+                val1 * 256 + int(payload[i].hex(), 16)
+            # highAlpha:
+            i = i + 1
+            val0 = int(payload[i].hex(), 16)
+            i = i + 1
+            val1 = int(payload[i].hex(), 16)
+            i = i + 1
+            w['highAlpha'] = val0 * 65536 + \
+                val1 * 256 + int(payload[i].hex(), 16)
+            # lowBeta:
+            i = i + 1
+            val0 = int(payload[i].hex(), 16)
+            i = i + 1
+            val1 = int(payload[i].hex(), 16)
+            i = i + 1
+            w['lowBeta'] = val0 * 65536 + \
+                val1 * 256 + int(payload[i].hex(), 16)
+            # highBeta:
+            i = i + 1
+            val0 = int(payload[i].hex(), 16)
+            i = i + 1
+            val1 = int(payload[i].hex(), 16)
+            i = i + 1
+            w['highBeta'] = val0 * 65536 + \
+                val1 * 256 + int(payload[i].hex(), 16)
+            # lowGamma:
+            i = i + 1
+            val0 = int(payload[i].hex(), 16)
+            i = i + 1
+            val1 = int(payload[i].hex(), 16)
+            i = i + 1
+            w['lowGamma'] = val0 * 65536 + \
+                val1 * 256 + int(payload[i].hex(), 16)
+            # midGamma:
+            i = i + 1
+            val0 = int(payload[i].hex(), 16)
+            i = i + 1
+            val1 = int(payload[i].hex(), 16)
+            i = i + 1
+            w['midGamma'] = val0 * 65536 + \
+                val1 * 256 + int(payload[i].hex(), 16)
+            print(w)
+        else:
+            pass
+        i = i + 1
 
 
 while True:
@@ -63,17 +176,19 @@ while True:
                 break
             # Read in the payload
             
-            payload = b''
+            payload = []
+            val = 0
             for i in range(plength):
                 t = ser.read(1)
-                payload += t
+                val += sum(x for x in t)
+                payload.append(t)
             # Verify its checksum
-            val = sum(b for b in payload)
+            # val = sum(b for b in payload)
             val &= 0xff
             val = ~val & 0xff
             chksum = ord(ser.read(1))
             if val ==  chksum:
-                parse_payload(payload)
+                handle_load(payload)
             else:
                 print('Bad checksum')
 
